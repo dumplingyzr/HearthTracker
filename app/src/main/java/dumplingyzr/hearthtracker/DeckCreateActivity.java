bd3db0f1baedc131fc2334b9f076c5f96e547061
dumplingyzr <yaozr@umich.edu>
2016-11-27 23:26:36 -0800

    Added spinners for filters(logic to be added yet)

--- app/src/main/java/dumplingyzr/hearthtracker/DeckCreateActivity.java
+++ app/src/main/java/dumplingyzr/hearthtracker/DeckCreateActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by dumplingyzr on 2016/11/27.
@@ -16,6 +18,8 @@ public class DeckCreateActivity extends Activity {
     private DeckCreateAdapter mCardPoolAdapter;
     private RecyclerView.LayoutManager mCurrDeckLayoutManager;
     private RecyclerView.LayoutManager mCardPoolLayoutManager;
+    private Spinner mCostFilterSpinner;
+    private Spinner mSetFilterSpinner;
 
     private Deck mDeck = new Deck();
 
@@ -34,5 +38,17 @@ public class DeckCreateActivity extends Activity {
         mCardPoolView.setLayoutManager(mCardPoolLayoutManager);
         mCurrDeckView.setAdapter(mCurrDeckAdapter);
         mCardPoolView.setAdapter(mCardPoolAdapter);
+
+        mCostFilterSpinner = (Spinner) findViewById(R.id.filter_cost);
+        ArrayAdapter<CharSequence> costFilterAdapter = ArrayAdapter.createFromResource(this,
+                R.array.card_cost, android.R.layout.simple_spinner_item);
+        costFilterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mCostFilterSpinner.setAdapter(costFilterAdapter);
+
+        mSetFilterSpinner = (Spinner) findViewById(R.id.filter_set);
+        ArrayAdapter<CharSequence> setFilterAdapter = ArrayAdapter.createFromResource(this,
+                R.array.card_set, android.R.layout.simple_spinner_item);
+        setFilterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSetFilterSpinner.setAdapter(setFilterAdapter);
     }
 }
