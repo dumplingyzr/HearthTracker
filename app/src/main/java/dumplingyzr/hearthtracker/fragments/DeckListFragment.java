87fccf311a8c124240665c614d99fb583dcebc7c
dumplingyzr <yaozr@umich.edu>
2017-01-02 18:58:52 -0800

    Improved UI on tablets

--- app/src/main/java/dumplingyzr/hearthtracker/fragments/DeckListFragment.java
+++ app/src/main/java/dumplingyzr/hearthtracker/fragments/DeckListFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
